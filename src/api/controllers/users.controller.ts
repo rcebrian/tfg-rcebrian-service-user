9bde6627f3009e331a28ab80d059cdd570c32503
rcebrian <rodrigocebriang@gmail.com>
2021-04-04 00:09:28 +0200

    ADD : crear usuario

--- src/api/controllers/users.controller.ts
+++ src/api/controllers/users.controller.ts
@@ -1,6 +1,8 @@
 import httpStatus from 'http-status';
 import { Request, Response } from 'express';
-import { User } from '../repository/mysql/mysql.repository';
+import { ValidationError } from 'sequelize';
+import bcrypt from 'bcrypt';
+import { User, Login } from '../repository/mysql/mysql.repository';
 
 export const findAll = (req: Request, res: Response) => {
   User.findAll().then((data) => {
@@ -12,9 +14,33 @@ export const findAll = (req: Request, res: Response) => {
   });
 };
 
-export const create = (req: Request, res: Response) => {
-  res.status(httpStatus.NOT_IMPLEMENTED).send();
+export const create = async (req: Request, res: Response) => {
+  const user = req.body;
+
+  User.create({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    phone: user.phone,
+    email: user.email,
+    address: user.address,
+    country: user.country,
+    postalCode: user.postalCode,
+    roleId: user.roleId,
+    login: {
+      passwordHash: await bcrypt.hash(user.password, 10),
+    },
+  }, {
+    include: Login,
+  }).then((data) => {
+    res.status(httpStatus.CREATED).json({ data });
+  }).catch((err: ValidationError) => {
+    res.status(httpStatus.BAD_REQUEST).json({ errors: err.message.split('\n') });
+  }).catch((err) => {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ error: err });
+  });
 };
+
 export const findById = (req: Request, res: Response) => {
   res.status(httpStatus.NOT_IMPLEMENTED).send();
 };
