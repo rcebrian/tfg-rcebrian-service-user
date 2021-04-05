e8fa8b499837f29366745f73572248e7e7fcf9ba
rcebrian <rodrigocebriang@gmail.com>
2021-04-05 22:33:30 +0200

    ADD : impl editar info de un user con validaciones

--- src/api/routes/users.route.ts
+++ src/api/routes/users.route.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import {
   findAll, create, findById, update, remove,
 } from '../controllers/users.controller';
-import { registerFormValidator, validator } from '../middlewares/validation.middleware';
+import { registerFormValidator, editUserInfoValidator, validator } from '../middlewares/validation.middleware';
 
 const router = Router();
 
@@ -10,7 +10,7 @@ router.route('/').get(findAll);
 router.route('/').post(registerFormValidator(), validator, create);
 
 router.route('/:userId([0-9]+)').get(findById);
-router.route('/:userId([0-9]+)').put(update);
+router.route('/:userId([0-9]+)').put(editUserInfoValidator(), validator, update);
 router.route('/:userId([0-9]+)').delete(remove);
 
 export { router as usersRoutes };
