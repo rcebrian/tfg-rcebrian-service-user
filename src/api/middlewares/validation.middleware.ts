ca569d0180220d2dbfe3811b4adfc4ed97ba13fc
rcebrian <rodrigocebriang@gmail.com>
2021-04-06 18:47:24 +0200

    FIX : no es error si no se actualiza un campo

--- src/api/middlewares/validation.middleware.ts
+++ src/api/middlewares/validation.middleware.ts
@@ -46,25 +46,25 @@ export const registerFormValidator = () => [
 ];
 
 export const editUserInfoValidator = () => [
-  body('firstName')
+  body('firstName').if(body('firstName').exists())
     .matches(/^[A-Za-z ]+$/)
     .withMessage('First name must be valid'),
-  body('lastName')
+  body('lastName').if(body('lastName').exists())
     .matches(/^[A-Za-z ]+$/)
     .withMessage('First name must be valid'),
-  body('phone')
+  body('phone').if(body('phone').exists())
     .isMobilePhone(['es-ES'])
     .withMessage('Mobile phone must be valid'),
-  body('email')
+  body('email').if(body('email').exists())
     .isEmail()
     .withMessage('Email must be valid'),
-  body('address')
+  body('address').if(body('address').exists())
     .matches(/^[A-Za-z0-9 ]+$/)
     .withMessage('Address must be valid'),
-  body('country')
+  body('country').if(body('country').exists())
     .matches(/^[A-Za-z ]+$/)
     .withMessage('Country must be valid'),
-  body('postalCode')
+  body('postalCode').if(body('postalCode').exists())
     .isPostalCode('ES')
     .withMessage('Postal code must be valid'),
 ];
